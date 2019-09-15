c0c58d4f6c722411544401e78b5dda707a8a70f3
Taeyoon <naming0617@gmail.com>
2019-09-16 00:55:51 +0900

    회원가입 코드 작성

--- src/main/java/me/naming/delieveryservice/service/UserService.java
+++ src/main/java/me/naming/delieveryservice/service/UserService.java
@@ -1,4 +1,23 @@
 package me.naming.delieveryservice.service;
 
+import me.naming.delieveryservice.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service("UserService")
 public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public String insertUserInfo(Map<String, String> userInfo) throws Exception{
+        String id = userInfo.get("id");
+        String password = userInfo.get("password");
+
+        userDao.insertUserInfo(id, password);
+        return "success";
+    }
+
 }
