27516dfaaa3b36cba68df4721eba772d86554ad5
Taeyoon <naming0617@gmail.com>
2019-09-16 00:52:29 +0900

    코드리뷰 반영

--- src/main/java/me/naming/delieveryservice/config/RedisConfig.java
+++ src/main/java/me/naming/delieveryservice/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -22,17 +23,12 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);      // Redis 연결 생성
-    }
-
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();        // RedisTemplate 생성(String, String 형으로 데이터 타입 지정)
-        redisTemplate.setConnectionFactory(redisConnectionFactory());               // ConnectionFactory 설정
-        redisTemplate.setKeySerializer(new StringRedisSerializer());                // Serializer 설정하지 않을 경우 Redis에 저장된 정보가 이상하다...?
-        redisTemplate.setValueSerializer(new StringRedisSerializer());              // 위의 설명과 동일하다.
-        return redisTemplate;
+    public StringRedisTemplate redisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(new LettuceConnectionFactory(redisHost, redisPort));
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        return stringRedisTemplate;
     }
 
 }
