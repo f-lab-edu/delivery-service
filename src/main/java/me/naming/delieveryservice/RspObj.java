545cbae18fdf1f050f5330030774efe5452d47db
Taeyoon <naming0617@gmail.com>
2019-09-27 11:07:47 -0500

    응답 클래스 생성

--- src/main/java/me/naming/delieveryservice/RspObj.java
+++ src/main/java/me/naming/delieveryservice/RspObj.java
@@ -1,4 +1,25 @@
 package me.naming.delieveryservice;
 
 public class RspObj {
+    public Boolean    hasError;
+    public int        code;
+    public String     msg;
+    public String     resultStr;
+    public Object     resultObject;
+    public long       resultLong;
+    public byte[]     resultByte;
+
+    public RspObj(){
+        this.hasError = false;
+        this.code = 200;
+        this.msg  = "none_data";
+    }
+
+    //생성과 동시에 성공값 셋팅
+    public RspObj(Object inObj){
+        this.code = 200;
+        this.hasError = false;
+        this.resultObject = inObj;
+        this.msg  = "none_data";
+    };
 }
