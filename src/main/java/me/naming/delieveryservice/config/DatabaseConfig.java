ce312cc1998b5988ff573c245fb531f3e0f67045
Taeyoon <naming0617@gmail.com>
2019-09-27 11:10:40 -0500

    주석 작성하기 피드백 내용 반영

--- src/main/java/me/naming/delieveryservice/config/DatabaseConfig.java
+++ src/main/java/me/naming/delieveryservice/config/DatabaseConfig.java
@@ -3,7 +3,6 @@ package me.naming.delieveryservice.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
@@ -18,25 +17,25 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")       // application.properties에 'spring.datasource.hikari'로 작성된 설정 내용을 갖고온다.
     public HikariConfig hikariConfig() {
         return new HikariConfig();
     }
 
     @Bean
     public DataSource dataSource(HikariConfig hikariConfig) {
-        DataSource dataSource = new HikariDataSource(hikariConfig);
+        DataSource dataSource = new HikariDataSource(hikariConfig);     // HikariConfig 빈 객체 주입
         return dataSource;
     }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource(dataSource);
-        sqlSessionFactory.setVfs(SpringBootVFS.class);
-        sqlSessionFactory.setTypeAliasesPackage("me.naming.delieveryservice.vo");
-        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/*.xml"));        // Mapper 파일 위치 셋팅
-        SqlSessionFactory sqlFactory = sqlSessionFactory.getObject();
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);                                    // 위에서 생성된 datasource 빈 객체 주입
+        sqlSessionFactoryBean.setVfs(SpringBootVFS.class);                                  // 스프링부트 가상파일 시스템을 설정
+        sqlSessionFactoryBean.setTypeAliasesPackage("me.naming.delieveryservice.vo");       // alias 대상 클래스가 위치한 패키지 경로 설정
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/*.xml"));        // Mapper 파일 위치 셋팅
+        SqlSessionFactory sqlFactory = sqlSessionFactoryBean.getObject();                   // SqlSessionFactoryBean을 활용해 Mybatis 구성 파일 없이 SqlSessionFactory 빌드
         sqlFactory.getConfiguration().setMapUnderscoreToCamelCase(true);
 
         return sqlFactory;
--- src/main/java/me/naming/delieveryservice/config/RedisConfig.java
+++ src/main/java/me/naming/delieveryservice/config/RedisConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,17 +23,16 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        return new LettuceConnectionFactory(redisHost, redisPort);      // 주소 값을 활용해 Redis 연결
     }
 
     @Bean
-    public StringRedisTemplate redisTemplate() {
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
-        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
-        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());      // 레디스 key 값에 대한 직렬화 설정
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());    // 레디스 value 값에 대한 직렬화 설정
         return stringRedisTemplate;
     }
 
 }
-
