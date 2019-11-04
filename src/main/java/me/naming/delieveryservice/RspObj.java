c8f7f4e0fd0474cbcf7f0ecbd1929cc146f8aea8
Taeyoon <naming0617@gmail.com>
2019-11-04 19:34:23 +0900

    enum 클래스 생성

--- src/main/java/me/naming/delieveryservice/RspObj.java
+++ src/main/java/me/naming/delieveryservice/RspObj.java
@@ -1,25 +1,30 @@
 package me.naming.delieveryservice;
 
-public class RspObj {
-    public Boolean    hasError;
-    public int        code;
-    public String     msg;
-    public String     resultStr;
-    public Object     resultObject;
-    public long       resultLong;
-    public byte[]     resultByte;
+public enum RspObj {
+  SUCCESS,
+  FAIL,
+  DELETE,
+  ERROR;
 
-    public RspObj(){
-        this.hasError = false;
-        this.code = 200;
-        this.msg  = "none_data";
-    }
-
-    //생성과 동시에 성공값 셋팅
-    public RspObj(Object inObj){
-        this.code = 200;
-        this.hasError = false;
-        this.resultObject = inObj;
-        this.msg  = "none_data";
-    };
+  //    public Boolean    hasError;
+  //    public int        code;
+  //    public String     msg;
+  //    public String     resultStr;
+  //    public Object     resultObject;
+  //    public long       resultLong;
+  //    public byte[]     resultByte;
+  //
+  //    public RspObj(){
+  //        this.hasError = false;
+  //        this.code = 200;
+  //        this.msg  = "none_data";
+  //    }
+  //
+  //    //생성과 동시에 성공값 셋팅
+  //    public RspObj(Object inObj){
+  //        this.code = 200;
+  //        this.hasError = false;
+  //        this.resultObject = inObj;
+  //        this.msg  = "none_data";
+  //    };
 }
