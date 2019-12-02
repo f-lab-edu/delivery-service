aa2d62ebce89b5c17b070a7b820182df1aae5eba
Taeyoon <naming0617@gmail.com>
2019-12-02 16:41:28 +0900

    테이블 변경으로 인한 코드 수정

--- src/main/java/me/naming/delieveryservice/controller/CustomerController.java
+++ src/main/java/me/naming/delieveryservice/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package me.naming.delieveryservice.controller;
 
-import java.util.concurrent.TimeUnit;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import me.naming.delieveryservice.dto.UserDTO;
 import me.naming.delieveryservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +14,15 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Lombok을 활용한 생성자 자동생성
@@ -70,11 +80,9 @@ public class CustomerController {
       @RequestBody UserLoginRequest userLoginRequest, HttpSession httpSession) throws Exception {
 
     UserDTO userDTO = userService.userLogin(userLoginRequest.getUserId(), userLoginRequest.getPassword());
-
     String sessionId = httpSession.getId();
     httpSession.setAttribute(userDTO.getUserId(), sessionId);
 
-    redisTemplate.opsForValue().set(userDTO.getUserId(), sessionId, 5, TimeUnit.MINUTES);
     return new ResponseEntity<>(ResponseResult.SUCCESS, HttpStatus.OK);
   }
 
