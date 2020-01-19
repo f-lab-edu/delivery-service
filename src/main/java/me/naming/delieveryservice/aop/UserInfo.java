dbe04a82b640632a8cf6a552ebc0d753df11bcca
Taeyoon <naming0617@gmail.com>
2020-01-19 22:10:58 +0900

    DB 테이블 구조 변경

--- src/main/java/me/naming/delieveryservice/aop/UserInfo.java
+++ src/main/java/me/naming/delieveryservice/aop/UserInfo.java
@@ -1,9 +1,10 @@
 package me.naming.delieveryservice.aop;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
-public @interface CheckPathVariableUserId {
+@Target(ElementType.PARAMETER)
+public @interface UserInfo {
 
 }
---
+++ src/main/java/me/naming/delieveryservice/dto/UserInfoDTO.java
@@ -0,0 +1,10 @@
+package me.naming.delieveryservice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserIdDTO {
+  private int userNum;
+  private String userId;
+  private String mobileNum;
+}
