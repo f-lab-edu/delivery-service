9fe3ee44a17e880419a53b2e225c2196479002c8
Taeyoon <naming0617@gmail.com>
2020-01-20 20:23:24 +0900

    RestTemplate 코드내용 수정

--- src/main/java/me/naming/delieveryservice/api/KakaoAPI.java
+++ src/main/java/me/naming/delieveryservice/api/KakaoAPI.java
@@ -25,7 +25,7 @@ public class KakaoAPI {
 
   // RestTemplate를 전역변수로 사용한 이유는 thread-safe하기 때문이다.
   // 참고 : https://www.notion.so/Delivery-Service-663d02f1c1264c23ba8c3d2ecf784793#91b74503c8f846bcaacd2a460ddb6e81
-  @Autowired RestTemplate kakaoRestTemplate;
+  @Autowired RestTemplate restTemplate;
 
   /**
    * '도로명 주소'를 좌표 값으로 변환
@@ -43,14 +43,14 @@ public class KakaoAPI {
         .queryParam("query", address)
         .build(false);
 
-    ResponseEntity<String> responseEntity = kakaoRestTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, new HttpEntity<String>(httpHeaders), String.class);
+    ResponseEntity<String> responseEntity = restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, new HttpEntity<String>(httpHeaders), String.class);
 
     JsonObject jsonObject = new Gson().fromJson(responseEntity.getBody(), JsonObject.class);
     JsonArray getDocuments = jsonObject.get("documents").getAsJsonArray();
 
     // statusCode는 200이나 결과 값이 존재하지 않을 수 있어서 Exception처리
     if(getDocuments.size() == 0)
-      throw new ArrayIndexOutOfBoundsException("kakaoAPI 결과 값이 존재하지 않습니다. 주소 값 체크("+address+")");
+      throw new ArrayIndexOutOfBoundsException("kakaoAPI 결과 값이 존재하지 않습니다. 검색한 주소는 '"+address+"' 입니다.");
 
     double longitude = getDocuments.get(0).getAsJsonObject().get("x").getAsDouble();
     double latitude = getDocuments.get(0).getAsJsonObject().get("y").getAsDouble();
--- src/main/java/me/naming/delieveryservice/config/RestTemplateConfig.java
+++ src/main/java/me/naming/delieveryservice/config/RestTemplateConfig.java
@@ -1,66 +1,21 @@
 package me.naming.delieveryservice.config;
 
-import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
-import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
-import java.io.IOException;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * RestTemplate을 Bean으로 생성하는 이유
- * Spring 프레임워크의 핵심 기능인 IoC를 활용해 객체를 관리해주기 위해 생성했다.
- * RestTemplate을 Bean으로 설정해도 괜찮은 이유는 RestTemplate은 스레드 세이프하기 때문에 여러 스레드가 동시에 접근해도
- * 원하는 결과 값을 출력할 수 있다.
+ * RestTemplate을 Bean으로 생성하는 이유 : Spring 프레임워크의 핵심 기능인 IoC를 활용해 객체를 관리해주기 위해 생성했다.
+ * RestTemplate을 Bean으로 설정해도 괜찮은 이유는 RestTemplate은 스레드 세이프하기 때문에 여러 스레드가 동시에 접근해도 원하는 결과 값을 출력할 수 있다.
+ * 참고
+ *  - https://spring.io/blog/2009/03/27/rest-in-spring-3-resttemplate
  */
 @Configuration
 public class RestTemplateConfig {
 
-  private class RestTemplateErrorHandler implements ResponseErrorHandler {
-
-    final String apiName;
-
-    public RestTemplateErrorHandler(String apiName) {
-      this.apiName = apiName;
-    }
-
-    @Override
-    public boolean hasError(ClientHttpResponse httpResponse) throws IOException {
-      return (httpResponse.getStatusCode().series() == CLIENT_ERROR
-          || httpResponse.getStatusCode().series() == SERVER_ERROR);
-    }
-
-    @Override
-    public void handleError(ClientHttpResponse httpResponse) throws IOException {
-
-      // handle SERVER_ERROR
-      if (httpResponse.getStatusCode().series() == SERVER_ERROR) {
-        throw new RuntimeException(apiName+" : API측 서버 에러 발생");
-
-        // handle CLIENT_ERROR
-      } else if (httpResponse.getStatusCode().series() == CLIENT_ERROR) {
-
-        if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND)
-          throw new RuntimeException(apiName+" : API URL 주소가 존재하지 않습니다");
-
-        if (httpResponse.getStatusCode() == HttpStatus.UNAUTHORIZED)
-          throw new RuntimeException(apiName+" : 클라이언트측에서 헤더에 AppKey를 추가하지 않았습니다");
-
-        if (httpResponse.getStatusCode() == HttpStatus.BAD_REQUEST)
-          throw new RuntimeException(apiName+" : 클라이언트측에서 쿼리 파라미터가 존재하지 않습니다");
-      }
-    }
-  }
-
   @Bean
-  public RestTemplate kakaoRestTemplate() {
-    RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-    RestTemplate restTemplate = restTemplateBuilder.errorHandler(new RestTemplateErrorHandler("카카오API")).build();
-
+  public RestTemplate restTemplate() {
+    RestTemplate restTemplate = new RestTemplate();
     return restTemplate;
   }
 }
