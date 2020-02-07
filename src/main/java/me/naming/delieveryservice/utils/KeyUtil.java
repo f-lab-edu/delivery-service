fdcd2456ad489447ce9ada2ea32688c2e7389148
Taeyoon <naming0617@gmail.com>
2020-02-07 10:56:16 +0900

    Key 관리 클래스 재수정

--- src/main/java/me/naming/delieveryservice/utils/KeyUtil.java
+++ src/main/java/me/naming/delieveryservice/utils/KeyUtil.java
@@ -1,8 +1,9 @@
 package me.naming.delieveryservice.utils;
 
-public class SessionKeyUtil {
+public class KeyUtil {
 
   public static final String DELIVERY_MAN_SESSION_KEY = "DeliveryManInfo";
   public static final String CUSTOMER_SESSION_KEY = "CustomerInfo";
 
+  public static final String FEE_INFO_CACHE_KEY = "FeeInfo";
 }
