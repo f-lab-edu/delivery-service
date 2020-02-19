0cc3352679e1221818523a1694b5e68645db3c30
Taeyoon <naming0617@gmail.com>
2020-02-19 12:24:22 +0900

    클래스명(SessionKeyUtil)변경

--- src/main/java/me/naming/delieveryservice/utils/SessionKeyUtil.java
+++ src/main/java/me/naming/delieveryservice/utils/SessionKeyUtil.java
@@ -1,9 +1,8 @@
 package me.naming.delieveryservice.utils;
 
-public class KeyUtil {
+public class SessionKeyUtil {
 
   public static final String DELIVERY_MAN_SESSION_KEY = "DeliveryManInfo";
   public static final String CUSTOMER_SESSION_KEY = "CustomerInfo";
 
-  public static final String FEE_INFO_CACHE_KEY = "FeeInfo";
 }
